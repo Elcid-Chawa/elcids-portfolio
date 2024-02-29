056cf4b13e0002292616551d09c157f673675262
Elcid-Chawa <elcidchawa@gmail.com>
2024-02-29 18:13:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ constants/index.ts
@@ -0,0 +1,150 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Stripe Payment",
+    Image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "GitHub",
+    src: "/github.svg",
+    url: "https://github.com/Elcid-Chawa",
+  },
+  {
+    name: "Instagram",
+    src: "/instagram.svg",
+    url: "https://www.instagram.com/elcid_c/",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+    url: "https://web.facebook.com/elcidchawa",
+  },
+  {
+    name: "Discord",
+    src: "/discord.svg",
+    url: "https://",
+  },
+  {
+    name: "LinkedIn",
+    src: "/linkedin.svg",
+    url: "https://www.linkedin.com/in/chawa-fabu-elcid-chawa-54b5223b/",
+  },
+];
+export const Projects = [
+  {
+    title: "Modern Nextjs Website",
+    text: "Working on modern web applciations using latest nextjs 14. Buildig landing pages.",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "Dashboards",
+    text: "Building sofisticated dashboards for systems to help monitor user activities.",
+    src: "/SpaceWebsite.png",
+  },
+  {
+    title: "Software Systems",
+    text: "Software systems for inventory management, payment gateways integration, mobile app development with Flutter ...",
+    src: "/WebPortfolio.png",
+  },
+  {
+    title: "Weather APP",
+    text: "Leverage the use of API's to integrate in app.",
+    src: "/Matrix.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
